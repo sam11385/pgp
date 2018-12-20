a0c808ef44c3a638794fe85c37c7ced7646dc5a5
sam11385 <samluedke@gmail.com>
2018-12-20 16:14:32 -0500

    add gitignore and query db from node.

--- index.js
+++ index.js
@@ -1,3 +1,28 @@
+var express = require('express');
+var app = express();
+const bodyParser = require('body-parser');
+
+let promise = require('bluebird');
+
+const initOptions = {
+  promiseLib: promise
+};
+
 const pgp = require('pg-promise')(initOptions);
 
+// Database connection parameters:
+const config = {
+  host: 'localhost',
+  port: 5432,
+  database: 'restaurant',
+  user: 'postgres'
+};
+// call our config file
 const db = pgp(config);
+
+// RUN SOME SQL!
+db.query('SELECT * FROM restaurant').then(function(results) {
+  results.forEach(function(r) {
+    console.log(r.id, r.name);
+  });
+});
